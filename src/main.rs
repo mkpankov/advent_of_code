7f73e523fec4971362f8dcca38d923cc36101dab
Michael Pankov <work@michaelpankov.com>
2022-12-21 17:22:35 +0300

    Implement stupid traversal

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ use std::{
     io::{self, Read, Write},
 };
 
-use petgraph::{dot::Dot, Graph};
+use petgraph::{
+    dot::Dot,
+    visit::{DfsPostOrder, Topo},
+    Graph,
+};
 
 #[derive(Debug)]
 struct Node {
@@ -87,6 +91,73 @@ fn main() -> Result<(), io::Error> {
     let output = format!("{:?}", Dot::new(&graph));
     let mut file = File::create("/tmp/graph.dot")?;
     file.write_all(output.as_bytes())?;
+
+    let root_index = node_indices["root"];
+    let mut visitor = DfsPostOrder::new(&graph, root_index);
+
+    let mut oper_1 = None;
+    let mut oper_2 = None;
+
+    let mut result = 0;
+    while let Some(i) = visitor.next(&graph) {
+        let n = graph.node_weight(i).unwrap();
+        println!("{n:?}");
+
+        match n.data {
+            Data::Value(v) => {
+                if oper_1.is_none() {
+                    oper_1 = Some(v);
+                } else if oper_2.is_none() {
+                    oper_2 = Some(v);
+                }
+            }
+            Data::Add => {
+                result = oper_1.unwrap() + oper_2.unwrap();
+                oper_1 = Some(result);
+                oper_2 = None;
+                *(graph.node_weight_mut(i).unwrap()) = Node {
+                    id: n.id.clone(),
+                    data: Data::Value(result),
+                };
+                let n = graph.node_weight(i).unwrap();
+                println!("After Add {n:?}");
+            }
+            Data::Sub => {
+                result = oper_1.unwrap() - oper_2.unwrap();
+                oper_1 = Some(result);
+                oper_2 = None;
+                *(graph.node_weight_mut(i).unwrap()) = Node {
+                    id: n.id.clone(),
+                    data: Data::Value(result),
+                };
+                let n = graph.node_weight(i).unwrap();
+                println!("After Sub {n:?}");
+            }
+            Data::Mul => {
+                result = oper_1.unwrap() * oper_2.unwrap();
+                oper_1 = Some(result);
+                oper_2 = None;
+                *(graph.node_weight_mut(i).unwrap()) = Node {
+                    id: n.id.clone(),
+                    data: Data::Value(result),
+                };
+                let n = graph.node_weight(i).unwrap();
+                println!("After Mul {n:?}");
+            }
+            Data::Div => {
+                result = oper_1.unwrap() / oper_2.unwrap();
+                oper_1 = Some(result);
+                oper_2 = None;
+                *(graph.node_weight_mut(i).unwrap()) = Node {
+                    id: n.id.clone(),
+                    data: Data::Value(result),
+                };
+                let n = graph.node_weight(i).unwrap();
+                println!("After Div {n:?}");
+            }
+        }
+    }
+    println!("{oper_1:?} {oper_2:?}");
 
     Ok(())
 }
